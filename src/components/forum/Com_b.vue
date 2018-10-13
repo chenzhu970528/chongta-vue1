3719f44987ec5518b85e91d56723f73bb666af4c
wmw <1968967200@qq.com>
2018-10-13 09:57:25 +0800

    提交

---
+++ src/components/forum/Com_b.vue
@@ -0,0 +1,18 @@
+<template>
+    <!--发帖-->
+  <div>
+    <input type="text">
+
+
+  </div>
+</template>
+
+<script>
+    export default {
+        name: "Com_b"
+    }
+</script>
+
+<style scoped>
+
+</style>
